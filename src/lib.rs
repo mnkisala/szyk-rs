1754ef6b3dea15f0ca2fb39cb91075f7b45c05af
nm <mnkisala@gmail.com>
2021-04-04 16:40:12 +0200

    more docs and package metadata

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+//! Generic topological sort algorithm (depth-first)
+//!
+//! # Examples
+//! ```
+//!     use szyk::*;
+//!
+//!     let result = topsort_values(
+//!         &[
+//!             Node::new("wooden pickaxe", vec!["planks", "sticks"], "Pickaxe"),
+//!             Node::new("planks", vec!["wood"], "Planks"),
+//!             Node::new("sticks", vec!["planks"], "Sticks"),
+//!             Node::new("wood", vec![], "Wood"),
+//!         ],
+//!         "wooden pickaxe",
+//!     );
+//!     assert_eq!(result, Ok(vec!["Wood", "Planks", "Sticks", "Pickaxe"]));
+//! ```
+
 #[derive(Debug, PartialEq)]
 pub struct Node<Id, Item>
 where
